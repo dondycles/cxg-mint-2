5ab1a9d1583eed173fa66e3f32faa1e8264fd6d3
dondycles <johnrod.dondoyano@gmail.com>
2022-12-13 23:25:50 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   components/background.js
    	modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,14 +15,9 @@ import { useMemo, useState } from "react";
 import styles from "../styles/Theme.module.css";
 import { parseIneligibility } from "../utils/parseIneligibility";
 import Head from "next/head";
-import Image from "next/image";
-
-import MorningBG from "../public/imgs/Morning/morning.webp";
-import RednightBG from "../public/imgs/Rednight/rednight.webp";
-import SunsetBG from "../public/imgs/Sunset/sunset.webp";
-import NightBG from "../public/imgs/Night/night.webp";
 
 import Socmed from "../components/socmeds";
+import Background from "../components/background";
 
 // Put Your Edition Drop Contract address from the dashboard here
 const myEditionDropContractAddress =
@@ -230,19 +225,12 @@ const Home: NextPage = () => {
         <link rel="shortcut icon" href="logo.ico" type="image/x-icon" />
       </Head>
       <Socmed />
-      <audio id="audio" className=" hidden" src="../aud/ost3.mp3"></audio>
-      <div
-        className="fixed flex top-0 left-0 right-0 bottom-0 -z-10 
-      "
-      >
-        <Image
-          className="blur-sm contrast-[1.2] brightness-75 object-cover scale-[1.02]"
-          src={NightBG}
-        />
-      </div>
+      <audio id="audioPlayer" className=" hidden" src="../aud/ost3.mp3"></audio>
+
+      <Background />
+
       <main className="flex absolute top-0 bottom-0 left-0 right-0 ">
         {/*Inital mint card*/}
-
         <div
           className={`flex flex-col items-center justify-center fixed top-0 left-0 right-0 bottom-0 w-[500px] h-[300px] bg-[#000000cd] m-auto rounded-2xl p-4 transition-all duration-1000 ease-in-out ${
             startMint
@@ -257,10 +245,10 @@ const Home: NextPage = () => {
               adipisicing elit.
             </p>
             <button
-              className="bg-black px-6 py-3  border-slate-900 border-2 rounded-lg hover:bg-slate-900 active:bg-slate-800 cursor-pointer"
+              className="bg-black px-6 py-3  border-red-800 border-2 rounded-lg hover:bg-red-800 active:bg-red-700 cursor-pointer"
               onClick={() => {
                 setStartMint(true);
-                var audio = document.querySelector("#audio");
+                var audio = document.querySelector("#audioPlayer");
                 audio.volume = 0.25;
                 audio.play();
               }}
@@ -281,11 +269,13 @@ const Home: NextPage = () => {
         >
           <div className="h-2 w-full bg-gradient-to-r from-transparent via-[#d80b31] to-transparent mt-0 mb-0 mx- auto rounded-lg bg-[length:200%] animate-redLineAnim"></div>
           {isLoading ? (
-            <p className="">Loading...</p>
+            <p className=" text-center m-auto  text-[40px] font-extrabold">
+              LOADING...
+            </p>
           ) : (
-            <div className=" m-auto">
+            <div className=" m-auto ">
               {/* Amount claimed so far */}
-              <div className="flex items-center justify-center gap-2">
+              <div className="flex items-center justify-center gap-2 p-2">
                 <p>Total Minted: </p>
                 <div className={styles.mintAreaRight}>
                   {claimedSupply ? (
@@ -305,19 +295,24 @@ const Home: NextPage = () => {
               claimConditions.data?.every(
                 (cc) => cc.maxClaimableSupply === "0"
               ) ? (
-                <div>
-                  <h2>
-                    This drop is not ready to be minted yet. (No claim condition
-                    set)
-                  </h2>
+                <div className="">
+                  <p className=" text-[20px] font-extrabold text-center">
+                    This drop is not ready to be minted yet.
+                    <br />
+                    (No claim condition set)
+                  </p>
                 </div>
               ) : (
                 <div className="">
-                  <div className="flex items-center justify-center gap-2">
+                  <div
+                    className={`flex items-center justify-center gap-2 
+                  ${isSoldOut ? " hidden" : " "}
+                  `}
+                  >
                     <p>Quantity:</p>
                     <div className=" flex my-auto w-fit gap-2  items-center h-10">
                       <button
-                        className=" bg-white text-black  border-white border-1 h-5 w-5 leading-5 rounded-full cursor-pointer text-2xl"
+                        className=" bg-black text-white  border-red-800 border-2 h-6 w-6 leading-4 rounded-full cursor-pointer text-2xl"
                         onClick={() => setQuantity(quantity - 1)}
                         disabled={quantity <= 1}
                       >
@@ -327,7 +322,7 @@ const Home: NextPage = () => {
                       <h4>{quantity}</h4>
 
                       <button
-                        className="bg-white text-black  border-white border-1 h-5 w-5 leading-5 rounded-full cursor-pointer text-2xl"
+                        className="bg-black text-white  border-red-800 border-2 h-6 w-6 leading-4 rounded-full cursor-pointer text-2xl"
                         onClick={() => setQuantity(quantity + 1)}
                         disabled={quantity >= maxClaimable}
                       >
@@ -338,8 +333,8 @@ const Home: NextPage = () => {
                   <div className="flex flex-col gap-2">
                     <div className="">
                       {isSoldOut ? (
-                        <div>
-                          <h2>Sold Out</h2>
+                        <div className="text-[35px] text-center font-extrabold">
+                          <h2>SOLD OUT</h2>
                         </div>
                       ) : (
                         <Web3Button
@@ -361,8 +356,12 @@ const Home: NextPage = () => {
                         </Web3Button>
                       )}
                     </div>
-                    <div className="w-full bg-black rounded-md border-2 border-gray-800 p-2 text-center hover:bg-slate-900 active:bg-slate-800 cursor-pointer">
-                      <button>MINT</button>
+                    <div
+                      className={`w-full font-extrabold bg-black rounded-md border-2 border-red-800 p-2 text-center hover:bg-red-800 active:bg-red-700 cursor-pointer
+                    ${isSoldOut ? " hidden " : " "}
+                    `}
+                    >
+                      <button>Mint</button>
                     </div>
                   </div>
                 </div>
